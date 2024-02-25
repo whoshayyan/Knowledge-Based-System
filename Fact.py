bea82f73803bd3b7290b47588a4b92a116ffd649
Hayan Aljabr <94853057+whoshayyan@users.noreply.github.com>
2024-02-25 12:52:46 +0300

    Add files via upload

---
+++ Fact.py
@@ -0,0 +1,57 @@
+from experta import *;
+
+# ================================
+#      This's the first facts
+# ================================
+#الانسجة في الثدي
+class Tissue_in_breast(Fact):
+    pass
+#الانسحاب في الثدي
+class Breast_withdrawal(Fact):
+    pass
+#التغير في الثدي
+class Change_in_breast(Fact):
+    pass
+#ألم في الابط او الثدي
+class Pain_in_armpit_and_breast(Fact):
+    pass
+#احمرار في الجلد
+class Redness_of_breast_skin(Fact):
+    pass
+#حكة في الجلد
+class Itching_or_rash(Fact):
+    pass
+#افرازات في الحلمة
+class Dump_the_nipple(Fact):
+    pass
+#تقشر في الجلد
+class Peeling_or_falling_skin(Fact):
+    pass
+
+
+# ================================
+#      This's the second facts
+# ================================
+class Age(Fact):
+    pass
+#الوراثة
+class Genetics(Fact):
+    pass
+
+class Previous_injury(Fact):
+    pass
+
+class Hormonal_compensatory_therapy(Fact):
+    pass
+
+class weight(Fact):
+    pass
+#الانجاب
+class Reproduction(Fact):
+    pass
+#الرضاعة الطبيعية
+class Breast_feeding(Fact):
+    pass
+#حبوب منع الحمل
+class Contraceptive_pills(Fact):
+    pass
